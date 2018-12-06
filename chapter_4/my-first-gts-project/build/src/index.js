24c3a81261a8e8d2291019dca2f0655eb8368003
Biharck Araujo <biharck@gmail.com>
2018-12-06 18:05:33 -0300

    removing build folder

--- chapter_4/my-first-gts-project/build/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const message = 'default message';
-function hello(word = message) {
-    return `Hello ${message}! `;
-}
-exports.hello = hello;
-//# sourceMappingURL=index.js.map
