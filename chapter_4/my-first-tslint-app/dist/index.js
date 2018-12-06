a94d0d6a53813d6f45d00ce57c135ee30d108268
Biharck Araujo <biharck@gmail.com>
2018-12-06 18:06:06 -0300

    removing dist folder

--- chapter_4/my-first-tslint-app/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var message = 'default message';
-function hello(word) {
-    if (word === void 0) { word = message; }
-    return "Hello " + message + "! ";
-}
-exports.hello = hello;
-//# sourceMappingURL=index.js.map
