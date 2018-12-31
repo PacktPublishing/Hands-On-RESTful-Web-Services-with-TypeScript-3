f880cc4fdac26d32f199a2b964aab14e5ecfa4b7
Biharck Araujo <biharck@gmail.com>
2018-12-31 01:12:04 -0200

    adding mongodb to the resolvers

--- chapter_13/order-ms/src/graphql/resolvers.ts
+++ chapter_13/order-ms/src/graphql/resolvers.ts
@@ -1,43 +1,12 @@
-import { OrderStatus } from '../models/orderStatus'
-
-const orders = [
-  {
-    id: 1,
-    userId: '1',
-    quantity: 2,
-    status: OrderStatus.Placed,
-    complete: false,
-  },
-  {
-    id: 2,
-    userId: '2',
-    quantity: 1,
-    status: OrderStatus.Placed,
-    complete: false,
-  },
-  {
-    id: 3,
-    userId: '3',
-    quantity: 1,
-    status: OrderStatus.Approved,
-    complete: false,
-  },
-  {
-    id: 4,
-    userId: '1',
-    quantity: 10,
-    status: OrderStatus.Delivered,
-    complete: true,
-  },
-]
+import { OrderModel } from '../schemas/order'
 
 export class OrderGraphQLResolvers {
   public getResolvers(): Object {
     return {
       Query: {
-        allOrders: () => orders,
-        listByOrderId: (root, args, context) => {
-          return orders.find(order => order.id === Number(args.id))
+        allOrders: async () => await OrderModel.find({}),
+        listByOrderId: async (root, args, context) => {
+          return await OrderModel.findById({ _id: args.id })
         },
       },
     }
